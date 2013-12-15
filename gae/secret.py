aa7182498904577cfb87d704f5690643e2a3d5dd
Semyon Maryasin <simeon@maryasin.name>
2013-12-16 03:00:21 +0400

    Second try.
    Now (in theory) access token must reach js.

--- gae/secret.py
+++ gae/secret.py
@@ -1,2 +1,2 @@
-client_secret = "Place your secret here"
+client_secret = "gWXYPoPL71h2_bH_vnsqwnIS"
 client_id = "203798620004-akugjt2cnhl6hnd3oua4eb4v8qegmriu.apps.googleusercontent.com"
--- gae/v1/auth.py
+++ gae/v1/auth.py
@@ -14,14 +14,32 @@ class AuthRedirector(webapp2.RequestHandler):
         self.response.location = url
         self.response.status_int = 302
 
-class AuthCallback(webapp2.RequestHandler):
-    def get(self):
-        # in case of error
+def query_json(url, data):
+    """
+    Query JSON data from Google server using POST request.
+    Returns only data and ignores result code.
+    """
+    if not (data is str):
+        data = urlencode(data)
+    try:
+        return json.loads(urllib2.urlopen(url, data).read())
+    except urllib2.HTTPError as e: # exception is a file-like object
+        return json.loads(e.read())
 
+def json_compactify(data):
+    return json.dumps(data, separators=(',',':')) # compact encoding
+
+class AuthCallback(webapp2.RequestHandler):
+    """
+    This page is called by Google when user finished auth process.
+    It receives state (currently unused), code (if success) or error (if failure).
+    Then it queries Google for access and refresh tokens and passes them in urlencode form
+    to intermediate static page, which will show status and pass data to js code.
+    """
+    def get(self):
         state = self.request.get("state")
         code = self.request.get("code")
         error = self.request.get("error")
-
         q = {
             "code": code,
             "client_id": client_id,
@@ -29,17 +47,39 @@ class AuthCallback(webapp2.RequestHandler):
             "redirect_uri": config.auth_redir_uri,
             "grant_type": "authorization_code",
         }
-        url = "https://accounts.google.com/o/oauth2/token"
-        print urlencode(q)
-        try:
-            result = json.loads(urllib2.urlopen(url, urlencode(q)).read())
-            self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
-            self.response.write(result)
-        except urllib2.HTTPError as e:
-            self.response.write("Error: " + str(e) + "\n" + e.read())
-            self.response.status_int = 500
+        result = query_json("https://accounts.google.com/o/oauth2/token", q)
+        url = (config.auth_success_page if "access_token" in result
+               else config.auth_failure_page) + "#" + urlencode(result)
+        self.response.location = url
+        self.response.status_int = 302
+        # return result as redirect to static page
+
+class AuthRefresh(webapp2.RequestHandler):
+    """
+    This page is used by client to refresh their access tokens.
+    An access token has lifetime of 1 hour, after that it becomes invalid and
+    needs to be refreshed.
+    So we receive refresh_token as a parameter
+    and return a new access_token with its lifetime as a json result.
+    """
+    def get(self):
+        refresh_token = self.request.get("refresh_token")
+        if not refresh_token:
+            self.response.status_int = 400
+            return
+        q = {
+            "refresh_token": refresh_token,
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "refresh_token",
+        }
+        result = query_json("https://accounts.google.com/o/oauth2/token", q)
+        self.response.headers['Content-Type'] = "application/json; charset=UTF-8"
+        self.response.write(json_compactify(result))
+        # return result as JSON
 
 application = webapp2.WSGIApplication([
     ('/v1/auth', AuthRedirector),
     ('/v1/auth/result', AuthCallback),
+    ('/v1/auth/refresh', AuthRefresh),
 ], debug=True)
