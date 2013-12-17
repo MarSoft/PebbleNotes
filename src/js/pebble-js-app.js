602eda3d26cc77e12bad08eab0d63500c8929b48
Semyon Maryasin <simeon@maryasin.name>
2013-12-18 02:12:58 +0400

    js: implemented querying all tasklists

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -68,11 +68,15 @@ var g_access_token = "";
 var g_refresh_token = "";
 
 /**
- * send query to googleTasks api
+ * send query to googleTasks api.
+ * Will automatically send error message to Pebble
+ * if anything goes wrong.
  *
  * endpoint: e.g. users/@me/lists or lists/.../tasks
  * params: {} with all args
  * success: callback(json)
+ * method: get or post or something else (defaults to Get)
+ * data: n/a for get method
  */
 function queryTasks(endpoint, params, success, method, data) {
 	url = "https://www.googleapis.com/tasks/v1/" + endpoint;
@@ -136,21 +140,79 @@ function displayError(text, code) {
 	console.log("Sending error msg to Pebble (Not implemented): " + text);
 	// TODO
 }
+function assert(val, message) {
+	if(!val) {
+		console.log("assertion failed");
+		displayError(message);
+	}
+}
+
+/**
+ * Sends appMessage to pebble; logs errors.
+ */
+function sendMessage(data) {
+	Pebble.sendAppMessage(data,
+		function(e) {
+			console.log("Message sent: " + e.data);
+		},
+	   	function(e) {
+			console.log("Failed to send message: transactionId=" + e.data.transactionId + ", error is "+e.error.message);
+		});
+}
+
+var g_tasklists = [];
+
+/* Main logic */
+function doGetAllLists() {
+	console.log("Querying all tasklists");
+	queryTasks("users/@me/lists", null, function(d) {
+		console.log("sending " + d.items.length + " items");
+		sendMessage({
+				code: 20, // array start/size
+				scope: 0,
+				count: d.items.length});
+		g_tasklists = []; // TODO: use it for caching
+		for(var i=0; i<d.items.length; i++) {
+			var l = d.items[i];
+			var lobj = {
+				id: l.id,
+				title: l.title,
+				size: -1
+			};
+			var id = g_tasklists.push(lobj);
+			sendMessage({
+					code: 21, // array item
+					scope: 0,
+					item: i,
+					listId: id,
+					title: lobj.title,
+					size: lobj.size}); // TODO
+		}
+		sendMessage({
+				code: 22, // array end
+				scope: 0,
+				count: g_tasklists.length}); // send resulting list length, just for any
+		console.log("sending finished");
+	});
+}
+function doGetOneList(listId) {
+	assert(false, "Not implemented yet");
+	queryTasks("lists/"+l.id+"/tasks", null, function(d) {
+		console.log("result: "+d);
+	});
+}
+function doGetTaskDetails(taskId) {
+	assert(false, "Not implemented yet");
+}
+function doChangeTaskStatus(taskId, isDone) {
+	assert(false, "Not implemented yet");
+}
 
 /* Initialization */
 Pebble.addEventListener("ready", function(e) {
 	console.log("JS is running. Okay.");
 	g_access_token = localStorage["access_token"];
-	console.log("Access token is "+g_access_token+".\n Testing it!");
-	queryTasks("users/@me/lists", null, function(d) {
-		console.log("result:"+d);
-		l = d.items[2];
-		console.log("querying "+l.title);
-		queryTasks("lists/"+l.id+"/tasks", null, function(d) {
-			console.log("result: "+d);
-		});
-	});
-	console.log("Okay");
+	doGetAllLists();
 });
 
 /* Configuration window */
@@ -177,4 +239,43 @@ Pebble.addEventListener("webviewclosed", function(e) {
 /* Messages */
 Pebble.addEventListener("appmessage", function(e) {
 	console.log("Received message: " + e.payload);
+	switch(e.payload.code) {
+	case 10: // get info
+		switch(e.payload.scope) {
+		case 0: // all lists
+			doGetAllLists();
+			break;
+		case 1: // one list
+			assert(e.payload.listId, "List ID was not provided for GetOneList query");
+			doGetOneList(e.payload.listId);
+			break;
+		case 2: // one task
+			assert(e.payload.taskId, "Task ID was not provided for GetTaskDetails query");
+			doGetTaskDetails(e.payload.taskId);
+			break;
+		default:
+			console.log("Unknown message scope "+e.payload.scope);
+			break;
+		}
+		break;
+	case 11: // change info
+		switch(e.payload.scope) {
+		case 2: // one task (here - "done" status)
+			assert(e.payload.taskId, "Task ID was not provided for ChangeTaskStatus query");
+			assert(e.payload.isDone, "New task status was not provided for ChangeTaskStatus query");
+			doChangeTaskStatus(e.payload.taskId, e.payload.isDone);
+			break;
+		case 0: // all lists
+		case 1: // one list
+			console.log("Cannot 'change' info for scope "+e.payload.scope);
+			break;
+		default:
+			console.log("Unknown message scope "+e.payload.scope);
+			break;
+		}
+		break;
+	default:
+		console.log("Unknown message code "+e.payload.code);
+		break;
+	}
 });
