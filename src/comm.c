d5f7866b7b6a50b4da95fa4fba609242f3608873
Semyon Maryasin <simeon@maryasin.name>
2013-12-19 12:26:35 +0400

    comm: check if tasklist is active (fix issue #24)

--- src/comm.c
+++ src/comm.c
@@ -39,9 +39,10 @@ void comm_query_task_details(int listId, int taskId) {
 }
 
 static void comm_in_received_handler(DictionaryIterator *iter, void *context) {
-	Tuple *tCode, *tMessage, *tScope, *tCount;
+	Tuple *tCode, *tMessage, *tScope;
 
 	tCode = dict_find(iter, KEY_CODE);
+	assert(tCode, "Message without code");
 	int code = (int)tCode->value->int32;
 	LOG("Message code: %d", code);
 
@@ -68,30 +69,35 @@ static void comm_in_received_handler(DictionaryIterator *iter, void *context) {
 	}
 
 	tScope = dict_find(iter, KEY_SCOPE);
+	assert(tScope, "No scope!");
 	int scope = (int)tScope->value->int32;
 	LOG("Message scope: %d", scope);
 
-	if(code == CODE_ARRAY_START) {
-		tCount = dict_find(iter, KEY_COUNT);
-		int count = (int)tCount->value->int32;
-		LOG("Items count: %d", count);
-		if(scope == SCOPE_LISTS)
+	if(scope == SCOPE_LISTS) {
+		if(!tl_is_active()) { // lists window is not active; ignoring message
+			LOG("Ignoring TaskLists-related message because they are inactive");
+			return;
+		}
+		if(code == CODE_ARRAY_START) {
+			int count = (int)dict_find(iter, KEY_COUNT)->value->int32;
+			LOG("Items count: %d", count);
 			tl_set_count(count);
-		else // others...
-			LOG("Unexpected scope: %d", scope);
-	} else if(code == CODE_ARRAY_ITEM) {
-		int i = (int)dict_find(iter, KEY_ITEM)->value->int32;
-		int listId = (int)dict_find(iter, KEY_LISTID)->value->int32;
-		char *title = dict_find(iter, KEY_TITLE)->value->cstring;
-		LOG("Item No: %d, Id=%d", i, listId);
-		tl_set_item(i, (TL_Item){
-			.id = listId,
-			.title = title,
-			.size = -1,
-		});
-	} else if(code == CODE_ARRAY_END) {
-	} else
-		LOG("Unexpected message code: %d", code);
+		} else if(code == CODE_ARRAY_ITEM) {
+			int i = (int)dict_find(iter, KEY_ITEM)->value->int32;
+			int listId = (int)dict_find(iter, KEY_LISTID)->value->int32;
+			char *title = dict_find(iter, KEY_TITLE)->value->cstring;
+			LOG("Item No: %d, Id=%d", i, listId);
+			tl_set_item(i, (TL_Item){
+				.id = listId,
+				.title = title,
+				.size = -1,
+			});
+		} else if(code == CODE_ARRAY_END) {
+		} else
+			LOG("Unexpected message code: %d", code);
+	} else {
+		LOG("Unexpected scope: %d", scope);
+	}
 }
 static void comm_in_dropped_handler(AppMessageResult reason, void *context) {
 	LOG("Message dropped: reason=%d", reason);
