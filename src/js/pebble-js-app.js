e189e7d63cbaf0f04ece0a4e9fbff04451342e68
Semyon Maryasin <simeon@maryasin.name>
2013-12-23 09:29:34 +0400

    gae, js: some fixes for logout

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -355,7 +355,7 @@ Pebble.addEventListener("ready", function(e) {
 /* Configuration window */
 Pebble.addEventListener("showConfiguration", function(e) {
 	console.log("Showing config window... new url");
-	Pebble.openURL("http://pebble-notes.appspot.com/v1/notes-config.html?option1=off");
+	Pebble.openURL("http://pebble-notes.appspot.com/v1/notes-config.html#"+JSON.stringify({access_token: g_access_token}));
 });
 Pebble.addEventListener("webviewclosed", function(e) {
 	console.log("webview closed: "+e.response);
