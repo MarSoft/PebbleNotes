31f98c794fcb5daab96e3a896207f723b6340177
Semyon Maryasin <simeon@maryasin.name>
2014-04-18 19:35:19 +0400

    gae: changed all pages to use HTTPS
    Access/refresh tokens may, if intercepted, grant access to user's
    private data.

--- gae/config.py
+++ gae/config.py
@@ -1,4 +1,4 @@
 auth_redir_uri = "https://pebble-notes.appspot.com/v1/auth/result"
-app_config_uri = "http://pebble-notes.appspot.com/v1/notes-config.html"
-auth_success_page = "http://pebble-notes.appspot.com/v1/msg-auth-success.html"
-auth_failure_page = "http://pebble-notes.appspot.com/v1/msg-auth-failed.html"
+app_config_uri = "https://pebble-notes.appspot.com/v1/notes-config.html"
+auth_success_page = "https://pebble-notes.appspot.com/v1/msg-auth-success.html"
+auth_failure_page = "https://pebble-notes.appspot.com/v1/msg-auth-failed.html"
