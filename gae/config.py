14dfdd4ba635b2a01ad8c29d583c2bc21cc1fe91
Semyon Maryasin <simeon@maryasin.name>
2014-04-18 22:13:34 +0400

    gae: config: use relative URLs for internal redirects

--- gae/config.py
+++ gae/config.py
@@ -2,10 +2,16 @@ import os
 
 # get our version id
 version = os.environ['CURRENT_VERSION_ID'].split('.')[0]
+
 # construct hostname for current version
 host = "https://%s.pebble-notes.appspot.com" % version
 
+# where user will be redirected after logging in with Google
 auth_redir_uri = host+"/auth/result"
-app_config_uri = host+"/notes-config.html"
-auth_success_page = host+"/msg-auth-success.html"
-auth_failure_page = host+"/msg-auth-failed.html"
+
+# uri for settings page (to be used in redirects)
+app_config_page = "/notes-config.html"
+# where to redirect after successful or failed auth
+# (details will be added as a #hashtag)
+auth_success_page = "/msg-auth-success.html"
+auth_failure_page = "/msg-auth-failed.html"
