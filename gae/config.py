61036058f7e4d0a3e139434ca0b0b7bed0ab6e57
Semyon Maryasin <simeon@maryasin.name>
2014-04-18 22:07:12 +0400

    gae: fixed URLs in config, added version determining

--- gae/config.py
+++ gae/config.py
@@ -1,4 +1,11 @@
-auth_redir_uri = "https://pebble-notes.appspot.com/v1/auth/result"
-app_config_uri = "https://pebble-notes.appspot.com/v1/notes-config.html"
-auth_success_page = "https://pebble-notes.appspot.com/v1/msg-auth-success.html"
-auth_failure_page = "https://pebble-notes.appspot.com/v1/msg-auth-failed.html"
+import os
+
+# get our version id
+version = os.environ['CURRENT_VERSION_ID'].split('.')[0]
+# construct hostname for current version
+host = "https://%s.pebble-notes.appspot.com" % version
+
+auth_redir_uri = host+"/auth/result"
+app_config_uri = host+"/notes-config.html"
+auth_success_page = host+"/msg-auth-success.html"
+auth_failure_page = host+"/msg-auth-failed.html"
