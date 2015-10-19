d8988ee362498f5562548b1993a007f3a3617e2b
Semyon Maryasin <simeon@maryasin.name>
2015-10-19 23:14:28 +0300

    Refactored some common methods to separate file

---
+++ gae/static/common.js
@@ -0,0 +1,29 @@
+function getHashParams() {
+	var hashParams = {};
+	var e,
+		a = /\+/g,  // Regex for replacing addition symbol with a space
+		r = /([^&;=]+)=?([^&;]*)/g,
+		d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+		q = window.location.hash.substring(1);
+	while (e = r.exec(q))
+		hashParams[d(e[1])] = d(e[2]);
+	return hashParams;
+}
+
+function getRetUrl() {
+	var query = location.search.substring(1);
+	var vars = query.split('&');
+	for(var i=0; i<vars.length; i++) {
+		var pair = vars[i].split('=');
+		if(pair[0] == 'return_to')
+			return decodeURIComponent(pair[1]);
+	}
+	return 'pebblejs://close#';
+}
+function closeConfig(data, nogo) {
+	data = data || '';
+	var url = getRetUrl() + data;
+	if(nogo)
+		return url;
+	window.location = url;
+}
