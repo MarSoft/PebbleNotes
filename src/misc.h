f156b364a887ce5519c6aa53beadf5a9c59adb95
Semyon Maryasin <simeon@maryasin.name>
2016-06-02 02:37:27 +0300

    Added MAX and MIN macros

--- src/misc.h
+++ src/misc.h
@@ -5,4 +5,7 @@
 #define assert(e, msg...) if(!(e)) { APP_LOG(APP_LOG_LEVEL_ERROR, msg); return; }
 #define assert_oom(e, msg...) if(!(e)) { APP_LOG(APP_LOG_LEVEL_ERROR, msg); sb_show("OOM"); }
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 #endif
