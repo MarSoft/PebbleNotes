0e4d77eaf517ddd0b3ea8b70a631d952dd9d46c1
Semyon Maryasin <simeon@maryasin.name>
2017-05-16 02:30:01 +0300

    Implementing backend for passcode approach

--- gae/auth.py
+++ gae/auth.py
@@ -2,6 +2,7 @@ import webapp2
 from urllib import urlencode
 import json
 import urllib2
+import random
 from google.appengine.api.memcache import Client as MemcacheClient
 
 from secret import client_id, client_secret
@@ -9,6 +10,15 @@ import config
 
 
 memcache = MemcacheClient()
+
+
+WORDS = (
+    'red green blue white brown violet purple black yellow orange '
+    'dog cat cow unicorn animal hedgehog chicken '
+    'task computer phone watch android robot apple '
+    'rambler rogue warrior king '
+    'jeans muffin cake bake cookie oven bread '
+).split()
 
 
 def query_json(url, data):
@@ -24,69 +34,31 @@ def query_json(url, data):
         return json.loads(e.read())
 
 
-class AuthCheck(webapp2.RequestHandler):
+class AuthRedirect(webapp2.RequestHandler):
     def get(self):
-        passcode = self.request.POST.get('passcode')
+        url = 'https://accounts.google.com/o/oauth2/auth?' + urlencode(dict(
+            client_id=client_id,
+            redirect_uri=config.auth_redir_uri,
+            response_type='code',
+            scope='https://www.googleapis.com/auth/tasks',
+            state='',
+            access_type='offline',
+            approval_prompt='force',
+            include_granted_scopes='true',
+        ))
+        self.response.location = url
+        self.response.status_int = 302
+
+
+class AuthCheck(webapp2.RequestHandler):
+    def post(self):
+        passcode = self.request.POST.get('passcode', '').lower()
         tokendata = memcache.get(passcode, namespace='passcode')
 
         self.response.headers['Content-Type'] = \
             "application/json; charset=UTF-8"
         self.response.write(tokendata or '{"error": "Incorrect token"}')
 
-AUTHPAGE = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta http-equiv="refresh" content="{interval}{url}">
-    </head>
-    <body>
-        <h1>Authentication</h1>
-        <p>Please go to <tt>{verification_url}</tt> in your browser
-        and enter this user code: <strong>{user_code}</strong>.</p>
-    </body>
-    </html>
-"""
-
-class AuthCodeHandler(webapp2.RequestHandler):
-    def get(self):
-        if not self.request.GET.get('user_code'):
-            # first request
-            ask = query_json(
-                'https://accounts.google.com/o/oauth2/device/code',
-                dict(
-                    client_id=client_id,
-                    scope='email profile https://www.googleapis.com/auth/tasks',
-                )
-            )
-            if 'user_code' not in ask:
-                self.response.write('ERROR: %s' % ask)
-                return
-            # first set it to empty
-            ask['url'] = ''
-            # now set it to meaningful, but with empty url value
-            ask['url'] = '; /auth?' + urlencode(ask)
-        else:
-            # poll request
-            ask = self.request.GET
-            result = query_json(
-                'https://www.googleapis.com/oauth2/v4/token',
-                dict(
-                    client_id=client_id,
-                    client_secret=client_secret,
-                    code=ask['device_code'],
-                    grant_type='http://oauth.net/grant_type/device/1.0',
-                ),
-            )
-            if result.get('error') != 'authorization_pending':
-                # done! Redirect to resulting page
-                url = (config.auth_success_page if "access_token" in result
-                       else config.auth_failure_page) + "#" + urlencode(result)
-                self.response.location = url
-                self.response.status_int = 302
-                return
-
-        self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(AUTHPAGE.format(**ask))
 
 class AuthPoller(webapp2.RequestHandler):
     def get(self):
@@ -97,8 +69,10 @@ class AuthPoller(webapp2.RequestHandler):
         self.response.location = url
         self.response.status_int = 302
 
+
 def json_compactify(data):
     return json.dumps(data, separators=(',', ':'))  # compact encoding
+
 
 class AuthCallback(webapp2.RequestHandler):
     """
@@ -122,11 +96,20 @@ class AuthCallback(webapp2.RequestHandler):
             "grant_type": "authorization_code",
         }
         result = query_json("https://accounts.google.com/o/oauth2/token", q)
+        if 'access_token' not in result:
+            self.response.write('ERROR: %s' % result)
+            return
+
+        words = random.shuffle(WORDS[:])
+        passcode = words[:5]
+        memcache.add(passcode, json_compactify(result), namespace='passcode',
+                     time=30*60)  # store for 30 minutes
         url = (config.auth_success_page if "access_token" in result
                else config.auth_failure_page) + "#" + urlencode(result)
         self.response.location = url
         self.response.status_int = 302
         # return result as redirect to static page
+
 
 class AuthRefresh(webapp2.RequestHandler):
     """
@@ -153,9 +136,10 @@ class AuthRefresh(webapp2.RequestHandler):
         self.response.write(json_compactify(result))
         # return result as JSON
 
+
 application = webapp2.WSGIApplication([
+    ('/auth', AuthRedirect),
     ('/auth/check', AuthCheck),
-    ('/auth', AuthCodeHandler),
     ('/auth/result', AuthCallback),
     ('/auth/refresh', AuthRefresh),
 ], debug=True)
